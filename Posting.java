c046da34eed453ab421d98882c21ba470698e37e
Nutchanon Phongoen <nutchanon.pho@gmail.com>
2014-03-10 17:50:13 +0700

    version 0.21

--- Posting.java
+++ Posting.java
@@ -2,22 +2,41 @@ import java.io.Serializable;
 import java.util.ArrayList;
 public class Posting implements Serializable, Comparable<Posting>{
  private int docID;
+<<<<<<< HEAD
  private int termFrequency;
  private static final long serialVersionUID = 42L;
  //Constructor
  public Posting(int docIDInput, int tf){
   docID = docIDInput;
   termFrequency = tf; 
+=======
+ private ArrayList<Integer> positionList;
+ private int termFrequency;
+ private static final long serialVersionUID = 42L;
+ public Posting(int docIDInput , ArrayList<Integer> positionInput){
+  docID = docIDInput;
+  positionList = positionInput;
+  termFrequency = positionList.size(); 
+>>>>>>> parent of 4b90b73... version 0.1
  }
  
  public int getDocID(){
   return docID;
  }
+<<<<<<< HEAD
  
  public int getTermFrequency(){
   return termFrequency;
  }
  /*
+=======
+ public ArrayList<Integer> getPositionList(){
+  return positionList;
+ }
+ public int getTermFrequency(){
+  return termFrequency;
+ }
+>>>>>>> parent of 4b90b73... version 0.1
  public boolean equals(Object obj){
   Posting other = (Posting) obj;
   if(docID == other.getDocID()){
@@ -32,6 +51,7 @@ public class Posting implements Serializable, Comparable<Posting>{
   else 
    return false;
   
+<<<<<<< HEAD
  }*/
  
  public String toString(){
@@ -44,6 +64,56 @@ public class Posting implements Serializable, Comparable<Posting>{
   positionList.add(input);
   termFrequency++;
  }*/
+=======
+ }
+ public String toString(){
+  String s = docID +"," + termFrequency + "<";
+  for(int i = 0;i<termFrequency;i++)
+  {
+   s+=positionList.get(i);
+   if(i != termFrequency -1) 
+    s+=",";
+  }
+  s+= ">";
+  return s;
+ }
+ public ArrayList<Integer> intersect(ArrayList<Integer> p2)
+ {
+  ArrayList<Integer> p1 = positionList;
+  ArrayList<Integer> answer = new ArrayList<Integer>();
+  int p1Index = 0;
+  int p2Index = 0;
+  while(p1Index < p1.size() && p2Index < p2.size())
+  {
+   if(p1.get(p1Index) == p2.get(p2Index))
+   {
+    answer.add(p1.get(p1Index));
+   }
+   else
+   {
+    if(p1.get(p1Index) < p2.get(p2Index))
+    {
+     answer.add(p1.get(p1Index));
+     answer.add(p2.get(p2Index));
+    }
+    else
+    {
+     answer.add(p2.get(p2Index));
+     answer.add(p1.get(p1Index));
+    }
+   }
+   p1Index++;
+   p2Index++;
+  }
+  return answer;
+ }
+ 
+ public void add(int input)
+ {
+  positionList.add(input);
+  termFrequency++;
+ }
+>>>>>>> parent of 4b90b73... version 0.1
 
  @Override
  public int compareTo(Posting arg) {
